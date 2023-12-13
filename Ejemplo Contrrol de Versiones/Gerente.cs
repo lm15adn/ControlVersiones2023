07bf6d260dd8aaee3241ebbc3120b57461fe8420
Luis Manuel Ardon Ramirez <LArdon@disagro.org>
2023-12-13 16:53:33 -0600

    t :wq odo

---
+++ Ejemplo Contrrol de Versiones/Gerente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using static Ejemplo_Contrrol_de_Versiones.Empleado;
+
+namespace Ejemplo_Contrrol_de_Versiones
+{
+    class Gerente:Empleado
+    {
+        private const double BONO_GERENTE = 0.15;
+        public Gerente()
+        {
+            
+        }
+
+        public Gerente(string tipoEmpleado, string nombre, double salario)
+        { 
+            Salario = salario;
+            Nombre = nombre;
+            TipoEmpleado = tipoEmpleado;
+        }
+
+        public override double CalcularSalario()
+        {
+            double BonoIncentivo = Salario * 0.05;
+            double BonoGerente = Salario * BONO_GERENTE;
+            return Salario +BonoIncentivo + BonoGerente;
+        }
+    }
+}
--- Ejemplo Contrrol de Versiones/Program.cs
+++ Ejemplo Contrrol de Versiones/Program.cs
@@ -2,5 +2,26 @@
 Console.WriteLine("Bienvenidos");
 Console.WriteLine("--Sistema de control de empleados");
 Console.WriteLine("\t 1. Agregar empleados");
+<<<<<<< HEAD
 Console.WriteLine("1. Listar empleados");
 Console.ReadLine();
+=======
+Console.WriteLine("\t 2. Listar empleados");
+Console.WriteLine("\t 3. Salir");
+Console.WriteLine("--SELECCIONE UNA OPCIÓN--");
+char Opcion = Char.Parse(Console.ReadLine());
+switch (Opcion)
+{
+    case '1':
+        Console.WriteLine("---Agregar Empleado---");
+        break;
+    case '2':
+        Console.WriteLine("---Listar Empleado---");
+        break;
+    default:
+        Console.WriteLine("Opción inválida");
+        break;
+
+}
+
+>>>>>>> 73ebb98 (Se añadió la clase gerente)
