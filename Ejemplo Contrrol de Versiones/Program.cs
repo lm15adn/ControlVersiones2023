524a81e9f08bfbf2adc00245bd4da24d71a95ab4
Luis Manuel Ardon Ramirez <LArdon@disagro.org>
2023-12-13 16:59:38 -0600

    Se corrigió la salida del programa v224

--- Ejemplo Contrrol de Versiones/Program.cs
+++ Ejemplo Contrrol de Versiones/Program.cs
@@ -2,10 +2,6 @@
 Console.WriteLine("Bienvenidos");
 Console.WriteLine("--Sistema de control de empleados");
 Console.WriteLine("\t 1. Agregar empleados");
-<<<<<<< HEAD
-Console.WriteLine("1. Listar empleados");
-Console.ReadLine();
-=======
 Console.WriteLine("\t 2. Listar empleados");
 Console.WriteLine("\t 3. Salir");
 Console.WriteLine("--SELECCIONE UNA OPCIÓN--");
@@ -15,13 +11,17 @@ switch (Opcion)
     case '1':
         Console.WriteLine("---Agregar Empleado---");
         break;
+
     case '2':
         Console.WriteLine("---Listar Empleado---");
         break;
+
+    case '3':
+        Environment.Exit(0);
+        
+        
     default:
         Console.WriteLine("Opción inválida");
         break;
 
 }
-
->>>>>>> 73ebb98 (Se añadió la clase gerente)
