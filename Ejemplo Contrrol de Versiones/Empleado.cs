aaa69cc98e11d0e63b6c2bce6ec9783be11e4b11
Luis Manuel Ardon Ramirez <LArdon@disagro.org>
2023-12-13 16:13:40 -0600

    Se agregó la clase empleado

---
+++ Ejemplo Contrrol de Versiones/Empleado.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_Contrrol_de_Versiones
+{
+    abstract class Empleado
+    {
+
+        public Empleado() { 
+        
+        }
+
+        protected Empleado(string tipoEmpleado, string nombre, char genero, double salario)
+        {
+            TipoEmpleado = tipoEmpleado;
+            Nombre = nombre;
+            Genero = genero;
+            Salario = salario;
+
+        }
+
+        public string TipoEmpleado { get; set; }
+
+        public string Nombre { get; set; }
+
+        public char Genero { get; set; }
+
+        public double Salario { get; set; }
+
+        public abstract double CalcularSalario();
+
+
+
+
+    }
+}
