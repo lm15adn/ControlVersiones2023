0ae4eff8019fffb47778b11289e988e553169247
Luis Manuel Ardon Ramirez <LArdon@disagro.org>
2023-12-13 16:52:41 -0600

    se añadio un nuevo campo al menu principal

--- Ejemplo Contrrol de Versiones/Program.cs
+++ Ejemplo Contrrol de Versiones/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Bienvenidos");
 Console.WriteLine("--Sistema de control de empleados");
 Console.WriteLine("\t 1. Agregar empleados");
 Console.WriteLine("1. Listar empleados");
+Console.ReadLine();
